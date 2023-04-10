46b3507e0d5892bf85bc67ab350ae766892a3c83
Theo <theo.lisin@gmail.com>
2023-04-11 00:45:36 +0300

    mv BaseRectangleInterpolation

--- src/interpolation/base.py
+++ src/interpolation/base.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from typing import List, Callable
 from supervisely.geometry.geometry import Geometry
-from supervisely import Rectangle
 
 
 class BaseInterpolation(object):
@@ -100,25 +99,3 @@ class BaseInterpolation(object):
             interp_v.append(geom)
 
         return interp_v
-
-
-class BaseRectangleInterpolation(BaseInterpolation):
-    def geometry_to_numpy(self, obj: Geometry) -> np.ndarray:
-        if not isinstance(obj, Rectangle):
-            raise TypeError("Can interpolate only Rectangles.")
-
-        left_top = [obj.left, obj.top]
-        right_bottom = [obj.right, obj.bottom]
-
-        return np.array([left_top, right_bottom])
-
-    def numpy_to_geometry(self, obj: np.ndarray) -> Geometry:
-        left_top = obj[0].astype(int)
-        right_bottom = obj[1].astype(int)
-        fig = Rectangle(
-            top=left_top[1],
-            left=left_top[0],
-            bottom=right_bottom[1],
-            right=right_bottom[0],
-        )
-        return fig
--- src/interpolation/rectangle.py
+++ src/interpolation/rectangle.py
@@ -1,8 +1,32 @@
 import numpy as np
 
 from typing import List
-from interpolation.base import BaseRectangleInterpolation
+from supervisely.geometry.geometry import Geometry
 from supervisely import Rectangle
+
+from interpolation.base import BaseInterpolation
+
+
+class BaseRectangleInterpolation(BaseInterpolation):
+    def geometry_to_numpy(self, obj: Geometry) -> np.ndarray:
+        if not isinstance(obj, Rectangle):
+            raise TypeError("Can interpolate only Rectangles.")
+
+        left_top = [obj.left, obj.top]
+        right_bottom = [obj.right, obj.bottom]
+
+        return np.array([left_top, right_bottom])
+
+    def numpy_to_geometry(self, obj: np.ndarray) -> Geometry:
+        left_top = obj[0].astype(int)
+        right_bottom = obj[1].astype(int)
+        fig = Rectangle(
+            top=left_top[1],
+            left=left_top[0],
+            bottom=right_bottom[1],
+            right=right_bottom[0],
+        )
+        return fig
 
 
 class LinearRectangleInterpolation(BaseRectangleInterpolation):
