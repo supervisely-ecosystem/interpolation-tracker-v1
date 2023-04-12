c222fb0fc5795af343e820ec3c7dec80e0c71ef9
Theo <theo.lisin@gmail.com>
2023-04-12 13:45:05 +0300

    Raise error if not enough figures

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -156,9 +156,8 @@ class InterpolationTracker(object):
         # all_frames = list(range(min(frames), max(frames)))
 
         if len(frames) < 2:
-            g.logger.info(f"Skip interpolation for object #{object_id}: not enought figures.")
-            self._notify(len(self.objects_id))
-            return
+            g.logger.info(f"Skip interpolation for object #{object_id}: not enough figures.")
+            raise ValueError(f"Skip interpolation: not enough figures for object #{object_id}.")
 
         interpol_geom = self.interp_model.interpolate(sorted_frames, sorted_figures, all_frames)
 
